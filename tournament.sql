6e7fa86e30b7cb053feccc98349a439e75c9e838
JRod6001 <Jared.Terrore@gmail.com>
2016-11-29 15:43:38 -0700

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -17,14 +17,39 @@ CREATE DATABASE tournament;
 -- Connect to database
 \c tournament
 
--- Delete existing table
-DROP TABLE IF EXISTS tour_table;
-
 -- Make a new table
-CREATE TABLE tour_table (
+CREATE TABLE players (
 	player_id serial,
 	player_name text,
-	wins integer,
-	matches integer);
+	PRIMARY KEY(player_id));
 
+--This table records all matches with a unique ID, and the winner/loser
+CREATE TABLE matches (
+	match_id serial,
+	winning_player integer references players(player_id),
+	losing_player integer references players(player_id));
+	
+-- Orders players from most to least number of wins
+CREATE VIEW player_wins 
+	AS SELECT players.player_id as player_id, count(matches.winning_player) as wins 
+	FROM players LEFT JOIN matches 
+	ON player_id = matches.winning_player 
+	GROUP BY players.player_id;
+	
+-- Counts how many maches each player has played
+CREATE VIEW player_matches 
+	AS SELECT players.player_id as player_id, count(matches) AS matches
+	FROM players LEFT JOIN matches 
+	ON  (players.player_id = matches.losing_player) 
+	OR (players.player_id = matches.winning_player) 
+	GROUP BY players.player_id
+	ORDER BY players.player_id DESC;
 
+-- A view containing player_id, player_name, total_wins, total_matches:
+CREATE VIEW player_standings
+	AS SELECT players.player_id, players.player_name,
+	player_wins.wins, player_matches.matches
+	FROM players, player_wins, player_matches
+	WHERE player_wins.player_id = players.player_id
+	AND players.player_id = player_matches.player_id
+	ORDER BY wins DESC;
